c756297ded6553d8a2051df13d6761eeefdd5665
My Name <me@example.com>
2019-07-17 16:09:50 +0200

    added an editor for sensorproxy.yml and finished up the logging page

--- api/server_http.py
+++ api/server_http.py
@@ -3,11 +3,14 @@ from pyramid.config import Configurator
 from pyramid.response import Response, FileResponse
 from pyramid.httpexceptions import exception_response
 
+from sensorproxy.sensors import *
+from inspect import * 
+
 import os, re, json, subprocess
 
 def sensorproxy_switch(request):
     if request.method == 'POST': 
-        if 'state' in request.json:
+        if 'state' in json.dumps(request.json):
             switch = request.json['state']
             res = subprocess.Popen(['sudo', 'systemctl', switch, 'sensorproxy'], stdout=subprocess.PIPE)
             res.wait()
@@ -20,7 +23,10 @@ def sensorproxy_switch(request):
                     ), 
                 }), 
                 charset='utf-8', 
-                content_type='application/json'
+                content_type='application/json',
+                headerlist=[
+                    ('Access-Control-Allow-Origin', '*'),
+                ]
             )    
         else:
             return Response(json.dumps({
@@ -39,26 +45,44 @@ def sensorproxy_switch(request):
             charset='utf-8', 
             content_type='application/json'
         )
-"""
-def opticals(request):
-    lines = []
-    opticals = []
 
-    with open('/pysensorproxy/sensorproxy/sensors/optical.py', 'r') as f:
-        for line in f:
-            lines.append(line)
+def list_of_sensors(request):
+    if request.method == 'GET':
+        sensors = {}
 
-    for i in range(len(lines)):
-        if "@register_sensor" in lines[i]:
-            optical = lines[i+1];
-            result = re.search('class (.*)\(', optical)
-            opticals.append(result.group(1))
-    return Response(json.dumps({
-        'opticals': opticals
-    }), charset='utf-8', content_type="application/json", headerlist=[
-            ('Access-Control-Allow-Origin', '*'),
-        ])
-"""
+        for sensor in base.classes:
+            # print(sensor)
+            sensors[sensor] = {}
+            for method in base.classes[sensor].__dict__:
+                if isfunction(base.classes[sensor].__dict__[method]):
+                    arguments = getfullargspec(base.classes[sensor].__dict__[method]).annotations
+                    sensors[sensor][method] = {}
+                    # print(method)
+                    # print(getfullargspec(base.classes[sensor].__dict__[method]).annotations)
+                    for argument in arguments:
+                        sensors[sensor][method][argument] = arguments[argument].__name__
+                        # print(argument)
+                        # print(arguments[argument].__name__)
+
+        return Response(json.dumps({
+            'sensors': sensors
+            }), 
+            charset='utf-8', 
+            content_type='application/json',
+            headerlist=[
+                ('Access-Control-Allow-Origin', '*'),
+            ]
+        )    
+    else: 
+        return Response(json.dumps({
+                'status': 501,
+                'message':'Invalid Method'
+            }), 
+            status_code=501, 
+            charset='utf-8', 
+            content_type='application/json'
+        )
+
 def sensorproxy_yml(request): 
     if request.method == 'GET':
         res = subprocess.Popen(['cat','/boot/sensorproxy.yml'], stdout=subprocess.PIPE)
@@ -75,7 +99,13 @@ def sensorproxy_yml(request):
         )  
     elif request.method == 'POST':
         if 'body' in request.json:
-            return Response(json.dumps(request.json), 
+            f = open("/boot/sensorproxy.yml", "w+")
+            f.write(request.json['body'])
+            f.close()
+            return Response(json.dumps({
+                'status': 0,
+                'message': 'File successfully overwritten!' 
+            }), 
                 charset='utf-8', 
                 content_type='application/json', 
                 headerlist=[
@@ -131,11 +161,11 @@ if __name__ == '__main__':
     with Configurator() as config:
         config.add_route('testpath', '/testpath')
         config.add_route('sensorproxy_switch', '/systemctl/switch')
-        # config.add_route('opticals', '/opticals')
-        config.add_route('sensorproxy_yml', '/sensorproxy_yml')
+        config.add_route('list_of_sensors', '/sensors')
+        config.add_route('sensorproxy_yml', '/yml/sensorproxy')
         config.add_view(testpath, route_name='testpath')
         config.add_view(sensorproxy_switch, route_name='sensorproxy_switch')
-        # config.add_view(opticals, route_name="opticals")
+        config.add_view(list_of_sensors, route_name="list_of_sensors")
         config.add_view(sensorproxy_yml, route_name="sensorproxy_yml")
         app = config.make_wsgi_app()
     serve(app, host='0.0.0.0', port=6500)
--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,97 +1,158 @@
-
-var state = {
-	sensor_script: '',
-	selected: {
-		wifi: true,
-		camera: ''
-	},
-	items: []
-};
-
-const Home = { 
+const YamlModifier = { 
 	template: '#yml-modifier-template',
 	data: function() {
-		return state
+		return {
+			sensorproxy_yml: '',
+			editor: null, 
+			sensors: {},
+			response: {},
+			windowHeight: 0,
+		}
 	},
 	mounted: async function() {
-		// socket.addEventListener('message', (event) => {
-		// 	state.items = JSON.parse(event.data).opticals;
-		// }); 
-		// let res = await axios.get('http://192.168.4.1:6500/opticals');
-		// console.log(res);
-		// state.items = res.data.opticals;
-		let res = await axios.get('http://192.168.4.1:6500/sensorproxy_yml');
+		this.editor = ace.edit("editor");
+		this.editor.setTheme("ace/theme/eclipse");
+		this.editor.session.setMode("ace/mode/yaml");
 
-		console.log(res);
-		state.sensor_script = '\n'+res.data.body.replace(/\\n/g, '\n');
-		this.generateScript();
+		let ysp = await fetch('http://192.168.4.1:6500/yml/sensorproxy', {
+			method: 'get',
+			credentials: 'same-origin',
+			mode: 'cors'
+		}) 
+
+		let sns = await fetch('http://192.168.4.1:6500/sensors', {
+			method: 'get',
+			credentials: 'same-origin',
+			mode: 'cors'
+		}) 
+
+		// let ysp = await axios.get('http://192.168.4.1:6500/yml/sensorproxy');
+		// let sns = await axios.get('http://192.168.4.1:6500/sensors');
+		
+		this.sensorproxy_yml = (await ysp.json()).body.replace(/\\n/g, '\n');
+		this.sensors = (await sns.json()).sensors;
+
+		console.log(this.sensors);
+
+		this.editor.session.setValue(this.sensorproxy_yml);
+
+		vm = this;
+
+		vm.windowHeight = window.innerHeight-40; 
+		window.addEventListener('resize', (evt) => {
+			vm.windowHeight = evt.target.innerHeight-40;
+			console.log(evt.target);
+		})
 	},
 	methods: {
-		generateScript: function() {
-			// console.log(
-			// 	'generateScript',this.selected.camera.value);
-			let gs = ''+
-			SensorScript.section.cam(this.selected.camera)+
-			SensorScript.section.log()+
-			(this.selected.wifi ? SensorScript.section.wifi() : '')+
-			SensorScript.section.lift();
-
-			// this.sensor_script = gs; 
-			// console.log(gs);
-		},
 		save: async function() {
-			let res = await axios.post('http://192.168.4.1:6500/sensorproxy_yml', {
-				body: this.sensor_script
-			},  {
+			let yml = await fetch('http://192.168.4.1:6500/yml/sensorproxy', {
+				method: 'post',
+				credentials: 'same-origin',
+				mode: 'cors',
 				headers: { 
-					'Content-Type': "application/json",
-					"Access-Control-Allow-Origin": "*", 
-				}
-			});
+					'Content-Type': "text/plain",
+				}, 
+				body: JSON.stringify({
+					body: this.editor.getValue()
+				}),
+			})
 
-			console.log(res);
+			this.response = await yml.json();
+			vm = this;
+			window.setTimeout(() => {
+				vm.response = {}
+			}, 3000)
 		},
-		saveApply: function() {
-			
+		saveApply: async function() {
+			let yml = await fetch('http://192.168.4.1:6500/yml/sensorproxy', {
+				method: 'post',
+				credentials: 'same-origin',
+				mode: 'cors',
+				headers: { 
+					'Content-Type': "text/plain",
+				}, 
+				body: JSON.stringify({
+					body: this.editor.getValue()
+				}),
+			})
+
+
+
+			let swtch = await fetch('http://192.168.4.1:6500/systemctl/switch', {
+				method: 'post',
+				credentials: 'same-origin',
+				mode: 'cors',
+				headers: { 
+					'Content-Type': "text/plain",
+				}, 
+				body: JSON.stringify({
+					state: 'restart'
+				}),
+			})
+
+			let message = (await yml.json()).message; 
+				message = message.substring(0, message.indexOf('!'));
+				message += ' and ' + (await swtch.json()).message;
+
+			this.response = {
+				message: message 
+			}; 
 		}
 	}
 }
-const Foo = { 
+
+const SensorLogs = { 
 	template: '#journalctl-logs-template',
 	data: function() {
 		return {
-			logs: ''
+			logs: '',
+			windowHeight: 0,
 		}
 	},
 	mounted: async function() {
 		let ws = new WebSocket('ws://192.168.4.1:6550/journalctl');
 		// const reader = new FileReader();
 		let logs = sessionStorage.getItem('sensorproxy-logs') ? sessionStorage.getItem('sensorproxy-logs') : '';
-		this.logs = '\n'+logs.trim(); 
-
-		console.log({ logs: logs });
+		this.logs = logs.trim(); 
 
 		vm = this;
-		ws.addEventListener('open', (evt) => {
-			console.log('WS: [OPEN]',evt);
-		})
-		ws.addEventListener('error', (err) => {
-			console.log('WS: [ERROR]',err.data);
-		})
-		ws.addEventListener('message', (evt) => {
-			// console.log(evt);
-			vm.logs += evt.data+'\n';
-		})
-		ws.addEventListener('close', (evt) => {
-			console.log('WS: [CLOSE]', evt);
-		})
+
+		vm.windowHeight = window.innerHeight-50; 
+		
+		ws.addEventListener('open', (evt) => console.log('WS: [OPEN]',evt))
+		ws.addEventListener('error', (err) => console.log('WS: [ERROR]',err))
+		ws.addEventListener('message', (evt) => vm.logs += evt.data+'\n')
+		ws.addEventListener('close', (evt) => console.log('WS: [CLOSE]', evt))
 
 		window.onbeforeunload = function() {
-			// ws.onclose = function () {}; // disable onclose handler first
 			ws.close();
-			// console.log(vm.logs);
 			sessionStorage.setItem('sensorproxy-logs', vm.logs);
 		};
+
+		window.addEventListener('resize', (evt) => {
+			vm.windowHeight = evt.target.innerHeight-50;
+			console.log(evt.target);
+		})
+	},
+	methods: {
+		sensorState: async function(state) {
+			let res = await fetch('http://192.168.4.1:6500/systemctl/switch', {
+				method: 'post',
+				credentials: 'same-origin',
+				mode: 'cors',
+				headers: { 
+					'Content-Type': "text/plain",
+				}, 
+				body: JSON.stringify({
+					state: state
+				}),
+			})
+		},
+		clearLogs: function() {
+			this.logs = '';
+			sessionStorage.setItem('sensorproxy-logs', '');
+		}
 	},
 	beforeRouteLeave: function (to, from, next) {
 		sessionStorage.setItem('sensorproxy-logs', this.logs);
@@ -104,47 +165,10 @@ const Foo = {
 const router = new VueRouter({
 	mode: 'history',
 	routes: [
-		{ path: '/', component: Home },
-		{ path: '/logs', component: Foo }
+		{ path: '/', component: YamlModifier },
+		{ path: '/logs', component: SensorLogs }
 	]
 })
-
-const SensorScript = {
-	content: '',
-	init : function() {
-		this.content = ''
-	},
-	section: {
-		cam: function(type) {
-			return type ? '\n'+
-				'cam:\n'+                            
-				`  type: ${type}\n`+
-				'  img_format: jpeg\n'
-			: '';
-		},		
-		log: function() {
-			return '\n'+
-				'log:\n'+                            
-				'  level: info\n'+
-				'  file_name: sensorproxy.txt\n';
-		},
-		wifi: function() {
-			return '\n'+
-				'wifi:\n'+                            
-				'  interface: wlan0\n'+
-				'  host_ap: true\n';
-		},
-		lift: function() {
-			return '\n'+
-				'lift:\n'+                            
-				'  ssid: nature40.liftsystem.709e\n'+
-				'  height: 30\n';
-		}
-	}
-}
-
-
-// const socket = new WebSocket('ws://127.0.0.1:6550/opticals'); 
 
 const app = new Vue({
 	router,
